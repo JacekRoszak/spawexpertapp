20c9a265bbd4cce281abd31b2f4e2f61f131a213
JacekRoszak <65806090+JacekRoszak@users.noreply.github.com>
2020-08-20 12:21:13 +0200

    make it better

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,14 @@
+class PagesController < ApplicationController
+  def offer
+  end
+
+  def realizations
+  end
+
+  def law
+  end
+
+  def about
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  root 'pages#offer'
+  get '/realizations', to: 'pages#realizations', as: 'realizations'
+  get '/law', to: 'pages#law', as: 'law'
+  get '/about', to: 'pages#about', as: 'about'
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
